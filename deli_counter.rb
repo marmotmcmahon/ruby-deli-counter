89d829997ebaeed705d20fc93a36887021f3b2d2
Marmot <Marmot@Marmo-Mathghamhain.local>
2018-09-09 13:59:30 -0500

    added take_a_number and now_serving methods

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,4 +4,14 @@ def line(array)
 	#shows everyone current place in line. Says empty if empty.
 end
 
+def take_a_number(array, customer)
+	puts "#{customer}: #{array.length + 1}"
+end
+
+def now_serving(array)
+	if array.empty?
+		puts "There is nobody waiting to be served!"
+	end
+end
+
 # END methods section
