7db99082994843b3bfa29610a1bf16f4e293d11a
Marmot <Marmot@Marmo-Mathghamhain.local>
2018-09-09 13:48:56 -0500

    line method started

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+# START methods section
+
+def line(array)
+	# shows current place in line. If empty, should say "The line is currently empty."
+	if array.length == 0
+		puts "The line is currently empty"
+	else
+		puts "You are currently number #{ array.length } in line."
+	end
+end
+# END methods section
