5f523427ebae86613f6c08c50a35c899b5fad990
Marmot <Marmot@Marmo-Mathghamhain.local>
2018-09-09 15:02:48 -0500

    now_serving method complete; added take_a_number testcases

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,18 @@ end
 def now_serving(array)
 	if array.empty?
 		puts "There is nobody waiting to be served!"
+	else
+		puts "Now serving: #{array[0]}"
 	end
+	array.shift
 end
 
 # END methods section
 
 katz_deli = []
+
+take_a_number(katz_deli, "Ada") #=> Welcome, Ada. You are number 1 in line.
+take_a_number(katz_deli, "Grace") #=> Welcome, Grace. You are number 2 in line.
+take_a_number(katz_deli, "Kent") #=> Welcome, Kent. You are number 3 in line.
+
+puts katz_deli
